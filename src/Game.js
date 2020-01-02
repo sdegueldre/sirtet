bab2108d616f654eb9125610bd4db6fe2f3e4d07
Samuel Degueldre <sad@odoo.com>
2020-01-02 18:26:02 +0100

    [REF] refactor shuffle to user better syntax and fix indentation
    My eyes are bleeding.

--- src/Game.js
+++ src/Game.js
@@ -246,11 +246,9 @@ function Canvas(){
 }
 
 function shuffle(arr){
-	for(let i = arr.length - 1; i > 0; i--){
-		const j = Math.floor(Math.random() * (i + 1));
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+  for(let i = arr.length - 1; i > 0; i--){
+	const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
 }
